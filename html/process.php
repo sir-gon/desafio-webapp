da26137441b278b811d3d251e24b25f73a0d6d30
Gonzalo Diaz Cruz <devel@gon.cl>
2017-01-30 04:40:10 -0300

    Happy path completado.

--- html/process.php
+++ html/process.php
@@ -7,6 +7,9 @@ require_once $base . '/config.php';
 
 use GuzzleHttp\Client;
 
+/**
+* QUERY API
+*/
 $body = new stdClass();
 $body->run = $_POST['run'] ? $_POST['run'] : '';
 $body->nombre =  $_POST['nombre'] ? $_POST['nombre'] : '';
@@ -24,10 +27,65 @@ $response = $client->post($conf['API_URL'],
     ['body' => json_encode($body)]
 );
 
-$json = json_decode($response->getBody()->getContents());
+$json = $response->getBody()->getContents();
+$jsonDecoded = json_decode($json);
 
-echo '<pre>' . var_export($_POST, true) . '</pre>';
+echo '<div><b>A&ntilde;os</b>: ' . $jsonDecoded->ageYears  . '</div>';
+echo '<div><b>Meses</b>: ' . $jsonDecoded->ageMonths  . '</div>';
+echo '<div><b>D&iacute;as</b>: ' . $jsonDecoded->ageDays  . '</div>';
 
-echo '<div><b>A&ntilde;os</b>: ' . $json->ageYears  . '</div>';
-echo '<div><b>Meses</b>: ' . $json->ageMonths  . '</div>';
-echo '<div><b>D&iacute;as</b>: ' . $json->ageDays  . '</div>';
+/**
+* Intentar crear BD
+*/
+
+echo '<div>* Almacenando en Base de Datos</div>';
+
+$client = new Client([]);
+
+$response = $client->put($conf['DB_URL'] . '/' . $conf['DB_NAME'], [
+    'auth' => [
+        $conf['DB_USER'],
+        $conf['DB_PASS']
+    ],
+    'http_errors' => false
+  ]
+);
+
+if($response->getStatusCode() === 200)
+{
+  echo '<div>* Base de datos creada</div>';
+}
+
+if($response->getStatusCode() === 412)
+{
+  echo '<div>* Base de datos existente</div>';
+}
+
+
+/**
+* Intentar guardar dato en BD
+*/
+
+$client = new Client([]);
+$url = $conf['DB_URL'] . '/' . $conf['DB_NAME'] . '/' . $jsonDecoded->run;
+$response = $client->put($url, [
+    'auth' => [
+        $conf['DB_USER'],
+        $conf['DB_PASS']
+    ],
+    'http_errors' => false,
+    'json' => $jsonDecoded
+  ]
+);
+
+
+if($response->getStatusCode() === 201)
+{
+  echo '<div>* Documento guardado en base de datos</div>';
+} else {
+
+  echo '<div>Documento enviado: <pre>' . $json . ' </pre></div>';
+  echo '<div>Codigo de estado de respuesta:<pre> ' . $response->getStatusCode()  . ' </pre></div>';
+  echo '<div>Contenido de estado de respuesta:<pre> ' . $response->getBody()  . ' </pre></div>';
+
+}
